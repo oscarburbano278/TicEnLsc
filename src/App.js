39f2c9c224ba0d1609cd129fbf59da26e0e9e57b
CristianWeAreBack <cristian.benavides@wearedev.co>
2024-07-25 13:52:06 -0500

    Add lazy loading for images in App.jstest

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
           <Route path="/acepR" element={<AcepReceptor />} />
           <Route path="/enviarA" element={<EnviarAudio />} />
           <Route path="*" element={<NotFound />} />
+          
         </Routes>
       </div>
     </Router>
