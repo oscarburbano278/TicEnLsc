be146ca3d0a9bc3aa6022ed3dad76b70a715e3f6
CristianWeAreBack <cristian.benavides@wearedev.co>
2024-07-25 13:24:06 -0500

    feat: Add NotFound component for handling 404 errors

--- src/App.js
+++ src/App.js
@@ -5,19 +5,18 @@ import Home from './Home';
 import ScannedPage from './ScannedPage';
 import AcepReceptor from './AcepReceptor';
 import EnviarAudio from './EnviarAudio';
+import NotFound from './NotFound';
 
 function App() {
   return (
     <Router>
       <div className="container">
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/scanned" element={<ScannedPage/>} />
-
-          
-          <Route path="/acepR" element={<AcepReceptor/>} />
-
-          <Route path="/enviarA" element={<EnviarAudio/>} />
+        <Route path="/" element={<Home />} />
+          <Route path="/scanned" element={<ScannedPage />} />
+          <Route path="/acepR" element={<AcepReceptor />} />
+          <Route path="/enviarA" element={<EnviarAudio />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </Router>
